8b634c4009660a7a48bd92b2e59eb05b85d0c889
HakanG <01guneshakan@gmail.com>
2025-10-28 21:45:39 +0300

    player wasd movement with force added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    private Rigidbody _playerRigidbody;
+    [SerializeField] private Transform _orientationTransform;
+    private float _horizontalInput, _verticalInput;
+    private Vector3 _moveDirection;
 
+    private void Awake()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _playerRigidbody.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-
+        SetInputs();
     }
+
+    private void FixedUpdate()
+    {
+        SetPlayerMovement();
+    }
+
+    private void SetInputs()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void SetPlayerMovement()
+    {
+        _moveDirection = _orientationTransform.forward * _verticalInput + _orientationTransform.right * _horizontalInput;
+        _playerRigidbody.AddForce(_moveDirection.normalized * 10f, ForceMode.Force);
+    }
+
+
 }
