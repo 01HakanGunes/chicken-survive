37ea199727df37afb3f5709e4e460a3952555aa3
HakanG <01guneshakan@gmail.com>
2025-10-29 17:16:42 +0300

    jump added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform _orientationTransform;
     private float _horizontalInput, _verticalInput;
     private Vector3 _moveDirection;
+    [SerializeField] private float _moveSpeed = 10f;
+    private KeyCode _jumpKey = KeyCode.Space;
+    [SerializeField] private float _jumpForce = 2f;
+    private bool _isGrounded = false;
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _playerHeight = 2f;
 
     private void Awake()
     {
@@ -28,13 +34,32 @@ public class PlayerController : MonoBehaviour
     {
         _horizontalInput = Input.GetAxisRaw("Horizontal");
         _verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(_jumpKey))
+        {
+            CheckGrounded();
+            if (_isGrounded)
+            {
+                SetPlayerJump();
+            }
+        }
     }
 
     private void SetPlayerMovement()
     {
         _moveDirection = _orientationTransform.forward * _verticalInput + _orientationTransform.right * _horizontalInput;
-        _playerRigidbody.AddForce(_moveDirection.normalized * 10f, ForceMode.Force);
+        _playerRigidbody.AddForce(_moveDirection.normalized * _moveSpeed, ForceMode.Force);
+    }
+
+    private void SetPlayerJump()
+    {
+        _playerRigidbody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+        _isGrounded = false;
     }
 
 
+    private void CheckGrounded()
+    {
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, _playerHeight / 2 + 0.1f, _groundLayer);
+    }
 }
